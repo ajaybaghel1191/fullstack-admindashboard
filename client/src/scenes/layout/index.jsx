9538b69c846f1be5da69c5f8ad894d33238f1dd9
ajaybaghel1191 <109842877+ajaybaghel1191@users.noreply.github.com>
2023-07-20 13:52:33 +0530

    first commit

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,33 @@
+import { Box, useMediaQuery } from "@mui/material";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+const Layout = () => {
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const [isSidebarOpen, setIsSidebarOpen]=useState(true);
+    const userId=useSelector((state)=>state.global.userId);
+     const {data} = useGetUserQuery(userId);
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+      user={data || {}}
+       isNonMobile={isNonMobile}
+       drawerWidth="250px"
+       isSidebarOpen={isSidebarOpen}
+       setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar 
+        user={data || {}}
+         isSidebarOpen={isSidebarOpen}
+         setIsSidebarOpen={setIsSidebarOpen}/>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
---
+++ server/routes/management.js
@@ -0,0 +1,8 @@
+import express  from "express";
+
+import { getAdmins, getUserPerformance } from "../controllers/management.js";
+const router = express.Router();
+router.get("/admins", getAdmins);
+router.get("/performance/:id", getUserPerformance);
+export default router;
+
